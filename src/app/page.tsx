e8a52a9050e76f719c76ac8ec414a12166a25619
shaikathaque <shaikathaque4@gmail.com>
2024-02-22 17:39:44 +0600

    render a list in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+const puns = [
+  'A',
+  'B',
+  "C",
+]
 
 export default function Home() {
+
+  function handleClick() {
+    console.log('Like button clicked')
+  }
+
   return (
     <main>
-      Hello World!
+
+      <h1 className="text-3xl font-bold underline">Welcome to Pun Generator!</h1>
+      <div>
+        {
+          puns.map((pun, i) => {
+            return (
+              <div key={i}>{pun}</div>
+            )
+          })
+        }
+      </div>
+
+
     </main>
   );
 }
