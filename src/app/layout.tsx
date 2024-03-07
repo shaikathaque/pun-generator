116cc0892aee65457e7f452d6d0199336e04eccb
shaikathaque <shaikathaque4@gmail.com>
2024-03-07 21:26:59 +0600

    chore: run formatter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Pun Generator",
+  title: 'Pun Generator',
   description: "The Internet's Favorite Puns!",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
-import prisma from "@/lib/prisma";
+import prisma from '@/lib/prisma';
 
 export default async function Home() {
-  const puns = await prisma.pun.findMany()
+  const puns = await prisma.pun.findMany();
 
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Welcome to Pun Generator!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Pun Generator!
+      </h1>
       <div>
-        {
-          puns.map((pun, i) => {
-            return (
-              <div key={i}>{pun.content}</div>
-            )
-          })
-        }
+        {puns.map((pun, i) => {
+          return <div key={i}>{pun.content}</div>;
+        })}
       </div>
-
-
     </main>
   );
 }
