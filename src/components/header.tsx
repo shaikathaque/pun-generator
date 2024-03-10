45e76ec74f99cad69a7d5701d97ee0d2e1ca379b
shaikathaque <shaikathaque4@gmail.com>
2024-03-10 21:21:04 +0600

    feat: header logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,15 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <nav className="flex h-16 items-center border-b-2 px-5">
       {/* Top left branding */}
-      <div>
-        <Link href="/">
-          <h1>Pun Generator</h1>
-        </Link>
-      </div>
+      <Link href="/" className="flex flex-row items-center gap-3">
+        <Image alt="app logo" src={'/app_logo.jpg'} width={40} height={40} />
+        <h1 className="font-semibold">Pun Generator</h1>
+      </Link>
 
       {/* Top right user nav */}
       <div className="ml-auto items-center">
